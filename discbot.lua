953060a45de7aed6ce66bc1a451e70289db0559f
Wintermourn <pundsack.tanner@dps-littlejohns.net>
2018-03-06 12:22:57 -0600

    Update discbot.lua

--- discbot.lua
+++ discbot.lua
@@ -1,13 +1,15 @@
 local discordia = require('discordia')
 local client = discordia.Client()
+local exreg = io.open("/discbot.dat/discbot_levels_v1")
 
 client:on('ready', function()
 	print('Logged in as '.. client.user.username)
 end)
 
 client:on('messageCreate', function(message)
-	if message.content == '!ping' then
-		message.channel:send('Pong!')
+	if message.content == '//+ wake up' then
+		message.channel:send([[Goog Morgning!
+It is I, DiscBot, at your service!]])
 	end
 end)
 
